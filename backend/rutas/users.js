4e40e7928b80c68312339e2e921dafa622b30cbc
Jesus Sanchez <josusssanher@gmail.com>
2024-06-20 10:51:51 -0600

    New dependence added

--- backend/rutas/users.js
+++ backend/rutas/users.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const Note = require('../modelos/users') // importacion del modelo
+const User = require('../modelos/users') // importacion del modelo
 
 // metodo get de inicio mi buen
 router.get('/users/get',(req, res) => {
-    Note.find()
+    User.find()
         .then(docs => {
             res.json(docs)
         })
@@ -12,6 +12,28 @@ router.get('/users/get',(req, res) => {
             console.error(err)
             res.status(500).json({ error: 'Error al obtener los usuarios' });
         })
+})
+
+// buscar usuario
+router.get('/users/login',(req,res) => {
+    const userName = req.query.username
+    if (!userName) {
+        return res.status(400).json({error:"no existe parametro de entrada"})
+    }
+
+    User.findOne({username:userName})
+    .then(user => {
+        if (!user) {
+            console.log('No existe una cuenta con ese nombre')
+            return res.status(400).json({error:"no hay usuario con ese nombre"})
+        }
+        console.log('tenemos al tonto')
+        res.json(user)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({error:"Ocurrio un error"})
+    })
 })
 
 module.exports = router
